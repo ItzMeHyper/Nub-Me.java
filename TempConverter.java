c476d39428ad356fd6be5838ba291b993170a464
Vaisakh Manu <133499771+ItzMeHyper@users.noreply.github.com>
2024-11-09 19:53:43 +0530

    Update TempConverter.java

--- TempConverter.java
+++ TempConverter.java
@@ -8,42 +8,44 @@ class convert extends JFrame implements ActionListener {
     JButton btn;
 
     public convert() {
-        setLayout(new FlowLayout());   
+        setLayout(new FlowLayout());
         setTitle("Hyper: Temperature Converter");
 
-        l1 = new JLabel("Enter temperature in Celsius:");         
-        t1 = new JTextField(20);                      
-        l2 = new JLabel("Temperature in farenheit:");  
-        t2 = new JTextField(20);         
+        l1 = new JLabel("Enter temperature in Celsius:");
+        t1 = new JTextField(20);
+        l2 = new JLabel("Temperature in farenheit:");
+        t2 = new JTextField(20);
 
-        btn = new JButton("Convert");  
+        btn = new JButton("Convert");
         t2.setEditable(false);
-        
-        add(l1);         
-        add(t1);                   
-        add(l2);
-        add(t2);        
-        add(btn);  
-        
-        btn.addActionListener(this);                 
 
-        setSize(400, 200);         
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);         
-        setVisible(true); 
+        add(l1);
+        add(t1);
+        add(l2);
+        add(t2);
+        add(btn);
+
+        btn.addActionListener(this);
+
+        setSize(400, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
     }
 
     public void actionPerformed(ActionEvent e) {
-        try {
-            double C = Double.parseDouble(t1.getText());
-            double Faren = (C * 9/5) + 32;
-            t2.setText(String.valueOf(Faren));
-        } catch (NumberFormatException ex) {             
-            t2.setText("Invalid input. Please enter number.");
-        }     
+        if (e.getSource() == btn) {
+            try {
+                double C = Double.parseDouble(t1.getText());
+                double Faren = (C * 9 / 5) + 32;
+                t2.setText(String.valueOf(Faren));
+            } catch (NumberFormatException ex) {
+                t2.setText("Invalid input. Please enter number.");
+            }
+        }
     }
 }
 
-public class TempConverter{
+public class TempConverter {
     public static void main(String[] args) {
         new convert();
     }
